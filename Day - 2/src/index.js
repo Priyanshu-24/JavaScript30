91001f2914762bb9d51330f78fe15cb1eaec89fc
Priyanshu Pandey <priyanshu24pandey@gmail.com>
2023-04-07 11:20:04 +0530

    day-2: hands rotation logic completed

--- Day - 2/src/index.js
+++ Day - 2/src/index.js
@@ -2,6 +2,20 @@ const secHand = document.querySelector(".sec-hand");
 const minHand = document.querySelector(".minute-hand");
 const hourHand = document.querySelector(".hour-hand");
 
-const getTime = () => {};
+const getTime = () => {
+  const date = new Date();
+
+  const sec = date.getSeconds();
+  const secDegree = (sec / 60) * 360 + 90;
+  secHand.style.transform = `rotate(${secDegree}deg)`;
+
+  const min = date.getMinutes();
+  const minDegree = (min / 60) * 360 + 90;
+  minHand.style.transform = `rotate(${minDegree}deg)`;
+
+  const hour = date.getHours();
+  const hourDegree = (hour / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegree}deg)`;
+};
 
 setInterval(getTime, 1000);
